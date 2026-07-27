f129821b833d863615e800bc328285aaa7c82cb1
PerryXu1 <perryxu6626@gmail.com>
2026-07-27 17:41:44 -0400

    feat: changes

--- acquire_waveform3.py
+++ acquire_waveform3.py
@@ -5,9 +5,9 @@ from classes.interface import Interface
 
 ELLIPTICITY = 15
 AZIMUTHAL = 0
-SHOTS = 100
+SHOTS = 10
 CHI2 = 0.3230
-VOLTS_PER_DIV = 5.3e-3
+VOLTS_PER_DIV = 300e-3
 
 CHANNEL = 2
 VISA_ADDRESS = "GPIB0::7::INSTR"  
@@ -16,11 +16,11 @@ WAVELENGTH = 1550e-9
 POLED_LENGTH = 0.3
 EFFECTIVE_DISTANCE = 41.1e-6
 CORE_INDEX = 1.45
-AC_VOLTAGE = 1006
+AC_VOLTAGE = 1150
 ELECTRODE_MATERIAL = "TUNGSTEN"
 TIME = 50
 POLING_VOLTAGE = 4500
-PIEZO_FREQUENCY = 120
+PIEZO_FREQUENCY = 60
 AC_FREQUENCY = 3000
 
 try:
@@ -29,14 +29,14 @@ try:
 
     scope.set_screen(channel=2,
                     volts_per_div=VOLTS_PER_DIV,
-                    time_per_div=1e-3,
-                    vertical_offset=3.5 * VOLTS_PER_DIV,
-                    horizontal_offset=5e-3,
+                    time_per_div=1.6e-3,
+                    vertical_offset=3 * VOLTS_PER_DIV,
+                    horizontal_offset=8e-3,
                     trigger_level=0,
                     ext_trigger=True)
 
     for i in range(SHOTS):
-        filename = f"potential_mode_mismatch_min_nopiezo_10_3000_{i + 1:03d}.txt"
+        filename = f"AC_voltage_dependence_60Hz_3000Hz_10V_{i + 1:03d}.txt"
 
         time, voltage = scope.acquire_signal(channel=2)
 
@@ -44,7 +44,7 @@ try:
             f"ELECTRODE={ELECTRODE_MATERIAL}, TIME={TIME}, POLING_VOLTAGE={POLING_VOLTAGE}, "
             f"WAVELENGTH={WAVELENGTH}, EFFECTIVE_DISTANCE={EFFECTIVE_DISTANCE}, "
             f"POLED_LENGTH={POLED_LENGTH}, CORE_INDEX={CORE_INDEX}, "
-            f"POLE=MIN, AC_VOLTAGE={AC_VOLTAGE}, AC_FREQUENCY={AC_FREQUENCY}\n"
+            f"POLE=MAX, AC_VOLTAGE={AC_VOLTAGE}, AC_FREQUENCY={AC_FREQUENCY}, PIEZO_FREQUENCY={PIEZO_FREQUENCY}\n"
             f"Time(s),Voltage(V)"
         )
         
