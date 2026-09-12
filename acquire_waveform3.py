9d0e028e93ad36b25884004adfe3aaffeeaa6eda
PerryXu1 <perryxu6626@gmail.com>
2026-09-12 17:43:49 -0400

    feat: changes

--- acquire_waveform3.py
+++ acquire_waveform3.py
@@ -6,7 +6,7 @@ import time as tm
 
 ELLIPTICITY = 15
 AZIMUTHAL = 0
-SHOTS = 10
+SHOTS = 20
 CHI2 = 0.3230
 
 CHANNEL = 2
@@ -14,12 +14,12 @@ VISA_ADDRESS = "GPIB0::7::INSTR"
 
 WAVELENGTH = 1550e-9
 POLED_LENGTH = 0.3
-EFFECTIVE_DISTANCE = 41.1e-6
+EFFECTIVE_DISTANCE = 31e-6
 CORE_INDEX = 1.45
 
 ELECTRODE_MATERIAL = "TUNGSTEN"
-TIME = 50
-POLING_VOLTAGE = 4500
+TIME = 30
+POLING_VOLTAGE = 3700
 
 # CHANGE
 AC_FREQUENCY = 6000
@@ -30,7 +30,7 @@ VOLTS_PER_DIV = 3e-3
 TIME_PER_DIV = 100e-3 / (PIEZO_FREQUENCY)
 
 # CHANGE
-AC_VOLTAGE = 643.7
+AC_VOLTAGE = 1025
 
 try:
     scope = Interface(instrument_num=1)
@@ -39,7 +39,7 @@ try:
     scope.set_screen(channel=2,
                     volts_per_div=VOLTS_PER_DIV,
                     time_per_div=TIME_PER_DIV,
-                    vertical_offset=3 * VOLTS_PER_DIV,
+                    vertical_offset=3.5 * VOLTS_PER_DIV,
                     horizontal_offset=5 * TIME_PER_DIV,
                     trigger_level=0,
                     ext_trigger=True)
@@ -50,7 +50,7 @@ try:
         time, voltage = scope.acquire_signal(channel=2)
 
         header_info = (
-            f"ELECTRODE={ELECTRODE_MATERIAL}, TIME={TIME}, POLING_VOLTAGE={POLING_VOLTAGE}, "
+            f"PURE SILICA, ELECTRODE={ELECTRODE_MATERIAL}, TIME={TIME}, POLING_VOLTAGE={POLING_VOLTAGE}, "
             f"WAVELENGTH={WAVELENGTH}, EFFECTIVE_DISTANCE={EFFECTIVE_DISTANCE}, "
             f"POLED_LENGTH={POLED_LENGTH}, CORE_INDEX={CORE_INDEX}, "
             f"POLE=MAX, AC_VOLTAGE={AC_VOLTAGE}, AC_FREQUENCY={AC_FREQUENCY}, PIEZO_FREQUENCY={PIEZO_FREQUENCY}\n"
